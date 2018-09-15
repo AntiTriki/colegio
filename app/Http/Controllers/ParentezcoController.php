ea22b64df9f6814e54a6a1e4aa480b04e674cdfe
AntiTriki <ne.i.on@hotmail.com>
2018-09-15 15:10:18 -0400

    Controllers y Modelos de BD v2

---
+++ app/Http/Controllers/ParentezcoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Parentezco;
+use Illuminate\Http\Request;
+
+class ParentezcoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Parentezco  $parentezco
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Parentezco $parentezco)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Parentezco  $parentezco
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Parentezco $parentezco)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Parentezco  $parentezco
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Parentezco $parentezco)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Parentezco  $parentezco
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Parentezco $parentezco)
+    {
+        //
+    }
+}
